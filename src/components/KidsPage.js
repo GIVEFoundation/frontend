cad1fbdc84c71b43afa57b377bc1f4c3ff1da2a0
unknown <anilsahu2010@gmail.com>
2018-05-30 14:11:13 +0530

    added kids page

--- src/components/KidsPage.js
+++ src/components/KidsPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from './Header';
+import Select from 'react-select';
 
+const options = [
+  { value: 'Aflatoun', label: 'Aflatoun' },
+  { value: 'British International School', label: 'British International School' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
 const KidsPage = (props) => {
+  props ={value:''}
 return (
 <article>
   <Header title="Kids"/>
   <h1 className="fw6 f3 f2-ns lh-title mt0 mb3">
     Kids
   </h1>
+  <Select options={options} />
 </article>);
 }
 export default KidsPage;
